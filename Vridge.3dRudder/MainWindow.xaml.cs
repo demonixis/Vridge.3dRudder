c1912f2b882fa58880ef030fa770cdb94b300d4e
Yann <comte.yannick@gmail.com>
2019-04-27 11:30:21 +0200

    More options. Crouch simulation.

--- Vridge.3dRudder/MainWindow.xaml.cs
+++ Vridge.3dRudder/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Threading;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 using VRE.Vridge.API.Client.Remotes;
 
 namespace Vridge._3dRudder
@@ -18,13 +20,14 @@ namespace Vridge._3dRudder
         private double X = 0;
         private double Y = 0;
         private double Z = 0;
-
+        private double Yaw = 0;
         private bool m_IsRunning;
-        public bool RotationEnabled { get; set; } = false;
-        public float MoveSpeed { get; set; } = 0.25f;
-        public float RotationSpeed { get; set; } = 0.25f;
-        public bool RudderEnabled { get; set; } = true;
-        public float HeadHeight { get; set; } = 1.8f;
+        private bool m_RotationEnabled = false;
+        private bool m_SimulateCrouch = true;
+        private float m_MoveSpeed = 0.25f;
+        private float m_RotationSpeed = 0.25f;
+        private bool m_RudderEnabled = true;
+        private float m_HeadHeight = 1.8f;
 
         public MainWindow()
         {
@@ -34,8 +37,16 @@ namespace Vridge._3dRudder
 
             InitializeComponent();
 
-            DataContext = this;
+            SetTextValue(HeadHeight, m_HeadHeight);
+            SetTextValue(MoveSpeed, m_MoveSpeed);
+            SetTextValue(RotationSpeed, m_RotationSpeed);
+            SetToggleValue(RotationEnabled, m_RotationEnabled);
+            SetToggleValue(RudderEnabled, m_RudderEnabled);
+            SetToggleValue(SimulateCrouch, m_SimulateCrouch);
         }
+
+        private void SetTextValue(TextBox textBox, object value) => textBox.Text = value.ToString();
+        private void SetToggleValue(CheckBox box, bool value) => box.IsChecked = value;
 
         public void Start()
         {
@@ -71,28 +82,48 @@ namespace Vridge._3dRudder
             StatusLabel.Text = "Stopped";
         }
 
+        private void Recenter()
+        {
+            X = 0;
+            Y = m_HeadHeight;
+            Z = 0;
+            Yaw = 0;
+        }
+
         private void ThreadLoop()
         {
+            Y = m_HeadHeight;
+
             while (m_IsRunning)
             {
-                if (RudderEnabled)
+                if (m_RudderEnabled)
                 {
                     var errorCode = Sdk3dRudder.GetAxes(0, m_AxesParamDefault, m_AxesValue);
 
                     if (errorCode == ErrorCode.Success)
                     {
-                        X += m_AxesValue.Get(Axes.LeftRight) * MoveSpeed;
-                        Y = HeadHeight;
-                        Z -= m_AxesValue.Get(Axes.ForwardBackward) * MoveSpeed;
+                        X += m_AxesValue.Get(Axes.LeftRight) * m_MoveSpeed;
+                        Z -= m_AxesValue.Get(Axes.ForwardBackward) * m_MoveSpeed;
 
-                        // TODO: Simulate Crouch using Y
+                        if (m_SimulateCrouch)
+                        {
+                            var y = m_AxesValue.Get(Axes.UpDown);
+                            if (y < -0.5f)
+                                Y = m_HeadHeight / 2;
+                            else
+                                Y = m_HeadHeight;
+                        }
+
+#if DEBUG
+                        Console.WriteLine($"X: {X}, Y: {Y}, Z: {Z}");
+#endif
 
                         if (m_VridgeRemote.Head != null)
                         {
-                            if (RotationEnabled)
+                            if (m_RotationEnabled)
                             {
-                                var rotationY = m_AxesValue.Get(Axes.Rotation) * MoveSpeed;
-                                m_VridgeRemote.Head.SetRotationAndPosition(rotationY, 0, 0, X, Y, Z);
+                                Yaw += m_AxesValue.Get(Axes.Rotation) * m_RotationSpeed;
+                                m_VridgeRemote.Head.SetRotationAndPosition(Yaw, 0, 0, X, Y, Z);
                             }
                             else
                                 m_VridgeRemote.Head.SetPosition(X, Y, Z);
@@ -106,5 +137,60 @@ namespace Vridge._3dRudder
 
         private void OnConnectClicked(object sender, RoutedEventArgs e) => Start();
         private void OnDisconnectClicked(object sender, RoutedEventArgs e) => Stop();
+        private void OnRecenterClicked(object sender, RoutedEventArgs e) => Recenter();
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Stop();
+            Application.Current.Shutdown();
+        }
+
+        private void HandleCheckbox(object sender, ref bool target)
+        {
+            var checkbox = (CheckBox)sender;
+
+            if (checkbox.IsChecked.HasValue)
+                target = checkbox.IsChecked.Value;
+            else
+                target = false;
+        }
+
+        private void HandleTextBoxFloat(object sender, ref float target)
+        {
+            var text = (TextBox)sender;
+
+            if (float.TryParse(text.Text, out float result))
+                target = result;
+        }
+
+        private void RudderEnabled_Click(object sender, RoutedEventArgs e)
+        {
+            HandleCheckbox(sender, ref m_RudderEnabled);
+        }
+
+        private void RotationEnabled_Click(object sender, RoutedEventArgs e)
+        {
+            HandleCheckbox(sender, ref m_RotationEnabled);
+        }
+
+        private void SimulateCrouch_Click(object sender, RoutedEventArgs e)
+        {
+            HandleCheckbox(sender, ref m_SimulateCrouch);
+        }
+
+        private void HeadHeight_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            HandleTextBoxFloat(sender, ref m_HeadHeight);
+        }
+
+        private void MoveSpeed_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            HandleTextBoxFloat(sender, ref m_MoveSpeed);
+        }
+
+        private void RotationSpeed_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            HandleTextBoxFloat(sender, ref m_RotationSpeed);
+        }
     }
 }
